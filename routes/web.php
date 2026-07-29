f3c34cef47752c41899a75716043d2323181ab8c
Yassine1000L <yassneyas11@gmail.com>
2026-07-30 01:25:33 +0200

    Debug: simplify route to string, remove all config:cache

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return 'Hello from Laravel';
+});
+
+Route::get('/letter', function () {
     return view('welcome');
 });
